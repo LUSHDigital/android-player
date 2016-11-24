9b789fe5754f77104985e329c9c16cb4ed95f9e2
Jamie Cruwys <jamie.cruwys@3sidedcube.com>
2016-11-24 14:37:42 +0000

    Added missing @Data annotation

--- lib/src/main/java/com/cube/lush/player/model/Programme.java
+++ lib/src/main/java/com/cube/lush/player/model/Programme.java
@@ -1,9 +1,12 @@
 package com.cube.lush.player.model;
 
+import lombok.Data;
+
 /**
  * @author Jamie Cruwys
  * @project lush-player-android-client
  */
+@Data
 public class Programme extends MediaContent
 {
 	private String url;

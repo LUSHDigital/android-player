58a2e52d72be6fac02e8cc6e2f135865838e130f
Jamie Cruwys <jamie.cruwys@3sidedcube.com>
2016-11-30 12:19:56 +0000

    Added search implementation

--- tv/src/main/java/com/cube/lush/player/SearchFragment.java
+++ tv/src/main/java/com/cube/lush/player/SearchFragment.java
@@ -4,12 +4,26 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v17.leanback.widget.ArrayObjectAdapter;
+import android.support.v17.leanback.widget.HeaderItem;
+import android.support.v17.leanback.widget.ListRow;
+import android.support.v17.leanback.widget.ListRowPresenter;
 import android.support.v17.leanback.widget.ObjectAdapter;
 import android.support.v17.leanback.widget.OnItemViewClickedListener;
 import android.support.v17.leanback.widget.Presenter;
 import android.support.v17.leanback.widget.Row;
 import android.support.v17.leanback.widget.RowPresenter;
 import android.support.v17.leanback.widget.SpeechRecognitionCallback;
+import android.text.TextUtils;
+
+import com.cube.lush.player.handler.ResponseHandler;
+import com.cube.lush.player.manager.SearchManager;
+import com.cube.lush.player.model.SearchResult;
+import com.cube.lush.player.presenter.SearchResultPresenter;
+
+import java.util.List;
 
 import static android.content.pm.PackageManager.PERMISSION_GRANTED;
 
@@ -20,6 +34,7 @@ public class SearchFragment extends android.support.v17.leanback.app.SearchFragm
                                                                                                OnItemViewClickedListener
 {
 	private static final int REQUEST_SPEECH = 0x00000010;
+	private ArrayObjectAdapter mRowsAdapter;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState)
@@ -38,6 +53,8 @@ public class SearchFragment extends android.support.v17.leanback.app.SearchFragm
 			});
 		}
 
+		mRowsAdapter = new ArrayObjectAdapter(new ListRowPresenter());
+		setSearchResultProvider(this);
 		setOnItemViewClickedListener(this);
 	}
 
@@ -66,7 +83,7 @@ public class SearchFragment extends android.support.v17.leanback.app.SearchFragm
 	@Override
 	public ObjectAdapter getResultsAdapter()
 	{
-		return null;
+		return mRowsAdapter;
 	}
 
 	@Override
@@ -78,7 +95,35 @@ public class SearchFragment extends android.support.v17.leanback.app.SearchFragm
 	@Override
 	public boolean onQueryTextSubmit(String query)
 	{
-		return false;
+		search(query);
+		return true;
+	}
+
+	private void search(@NonNull String query)
+	{
+		if (TextUtils.isEmpty(query))
+		{
+			return;
+		}
+
+		SearchManager.getInstance().search(query, new ResponseHandler<SearchResult>()
+		{
+			@Override public void onSuccess(@NonNull List<SearchResult> items)
+			{
+				mRowsAdapter.clear();
+
+				ArrayObjectAdapter searchAdapter = new ArrayObjectAdapter(new SearchResultPresenter());
+				searchAdapter.addAll(0, items);
+				ListRow searchRow = new ListRow(new HeaderItem("Search Results"), searchAdapter);
+
+				mRowsAdapter.add(searchRow);
+			}
+
+			@Override public void onFailure(@Nullable Throwable t)
+			{
+				mRowsAdapter.clear();
+			}
+		});
 	}
 
 	@Override

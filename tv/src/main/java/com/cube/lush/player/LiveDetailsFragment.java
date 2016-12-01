074603179a73cb38f0029feb8fd2730004819016
Tim Matthews <tim@3sidedcube.com>
2016-12-01 12:27:23 +0000

    Added a fragment to show the live channel

---
+++ tv/src/main/java/com/cube/lush/player/LiveDetailsFragment.java
@@ -0,0 +1,63 @@
+package com.cube.lush.player;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v17.leanback.app.BrowseFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.cube.lush.player.adapter.BasicMainFragmentAdapter;
+
+/**
+ * Created by tim on 01/12/2016.
+ */
+public class LiveDetailsFragment extends Fragment implements BrowseFragment.MainFragmentAdapterProvider
+{
+	private BrowseFragment.MainFragmentAdapter<LiveDetailsFragment> mMainFragmentAdapter;
+
+	private PlaybackFragment playbackFragment;
+	private MediaDetailsFragment detailsFragment;
+
+	@Nullable
+	@Override
+	public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState)
+	{
+		View view = inflater.inflate(R.layout.fragment_live, container, false);
+
+		// Show live playback in the background
+        if (getChildFragmentManager().findFragmentById(R.id.background_container) == null)
+        {
+	        playbackFragment = new PlaybackFragment();
+            getChildFragmentManager().beginTransaction().replace(R.id.background_container, playbackFragment).commit();
+        }
+        else
+        {
+            playbackFragment = (PlaybackFragment) getChildFragmentManager().findFragmentById(R.id.background_container);
+        }
+
+		// Show media details in the foreground
+        if (getChildFragmentManager().findFragmentById(R.id.foreground_container) == null)
+        {
+	        detailsFragment = new MediaDetailsFragment();
+            getChildFragmentManager().beginTransaction().replace(R.id.foreground_container, detailsFragment).commit();
+        }
+        else
+        {
+            detailsFragment = (MediaDetailsFragment) getChildFragmentManager().findFragmentById(R.id.foreground_container);
+        }
+
+		return view;
+	}
+
+	@Override
+	public BrowseFragment.MainFragmentAdapter<LiveDetailsFragment> getMainFragmentAdapter()
+	{
+		if (mMainFragmentAdapter == null)
+		{
+			mMainFragmentAdapter = new BasicMainFragmentAdapter<>(this);
+		}
+		return mMainFragmentAdapter;
+	}
+}

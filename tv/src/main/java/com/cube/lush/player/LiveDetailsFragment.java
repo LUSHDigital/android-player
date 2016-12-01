1b8e0d11cc158f47bccbf9eea66b651b570fbd67
Tim Matthews <tim@3sidedcube.com>
2016-12-01 13:38:25 +0000

    Queue the live content up in the live details fragment

--- tv/src/main/java/com/cube/lush/player/LiveDetailsFragment.java
+++ tv/src/main/java/com/cube/lush/player/LiveDetailsFragment.java
@@ -2,6 +2,7 @@ package com.cube.lush.player;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v17.leanback.app.BrowseFragment;
 import android.view.LayoutInflater;
@@ -9,6 +10,11 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.cube.lush.player.adapter.BasicMainFragmentAdapter;
+import com.cube.lush.player.handler.ResponseHandler;
+import com.cube.lush.player.manager.MediaManager;
+import com.cube.lush.player.model.MediaContent;
+
+import java.util.List;
 
 /**
  * Created by tim on 01/12/2016.
@@ -47,6 +53,24 @@ public class LiveDetailsFragment extends Fragment implements BrowseFragment.Main
         {
             detailsFragment = (MediaDetailsFragment) getChildFragmentManager().findFragmentById(R.id.foreground_container);
         }
+
+        MediaManager.getInstance().getLiveContent(new ResponseHandler<MediaContent>()
+        {
+	        @Override
+	        public void onSuccess(@NonNull List<MediaContent> items)
+	        {
+		        if (!items.isEmpty() && items.get(0) != null)
+		        {
+					playbackFragment.queuePlaylist(items.get(0).getId());
+		        }
+	        }
+
+	        @Override
+	        public void onFailure(@Nullable Throwable t)
+	        {
+
+	        }
+        });
 
 		return view;
 	}

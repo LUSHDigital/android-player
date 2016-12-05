645a09655a8ee24d57e62924d827fb5fb6778659
Jamie Cruwys <jamie.cruwys@3sidedcube.com>
2016-12-05 15:16:47 +0000

    Added playback activity arguments

--- tv/src/main/java/com/cube/lush/player/PlaybackActivity.java
+++ tv/src/main/java/com/cube/lush/player/PlaybackActivity.java
@@ -1,9 +1,34 @@
 package com.cube.lush.player;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.text.TextUtils;
 
 public class PlaybackActivity extends LushActivity
 {
+	public static final String ARGUMENT_PLAYBACK_METHOD = "playback_method";
+	public static final String ARGUMENT_PLAYBACK_METHOD_VALUE = "playback_method_value";
+
+	public static Intent getIntent(@NonNull Context context, @Nullable PlaybackMethod playbackMethod, @Nullable String playbackMethodValue)
+	{
+		Intent intent = new Intent(context, PlaybackActivity.class);
+
+		if (playbackMethod != null)
+		{
+			intent.putExtra(ARGUMENT_PLAYBACK_METHOD, playbackMethod);
+		}
+
+		if (!TextUtils.isEmpty(playbackMethodValue))
+		{
+			intent.putExtra(ARGUMENT_PLAYBACK_METHOD_VALUE, playbackMethodValue);
+		}
+
+		return intent;
+	}
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{

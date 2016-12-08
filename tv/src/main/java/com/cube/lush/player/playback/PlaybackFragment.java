386f07c66f12c2746ca740be2d83b822680ffa6c
Tim Matthews <tim@3sidedcube.com>
2016-12-08 14:50:11 +0000

    Handle correctly seeking live playback and automatically progressing to the next show

--- tv/src/main/java/com/cube/lush/player/playback/PlaybackFragment.java
+++ tv/src/main/java/com/cube/lush/player/playback/PlaybackFragment.java
@@ -2,8 +2,10 @@ package com.cube.lush.player.playback;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,8 +21,13 @@ import com.brightcove.player.model.Video;
 import com.brightcove.player.view.BaseVideoView;
 import com.brightcove.player.view.BrightcovePlayerFragment;
 import com.cube.lush.player.R;
+import com.cube.lush.player.handler.ResponseHandler;
 import com.cube.lush.player.manager.MediaManager;
+import com.cube.lush.player.model.MediaContent;
+import com.cube.lush.player.model.VideoInfo;
 import com.nostra13.universalimageloader.core.ImageLoader;
+
+import java.util.List;
 
 /**
  * Uses the Brightcove SDK player to playback Lush-related content, including playlists, specific videos, or remote files.
@@ -81,6 +88,48 @@ public class PlaybackFragment extends BrightcovePlayerFragment
 	}
 
 	/**
+	 * Start playing the live Lush content
+	 */
+	public void playLive()
+	{
+		brightcoveVideoView.stopPlayback();
+
+		MediaManager.getInstance().getLiveContent(new ResponseHandler<MediaContent>()
+		{
+			@Override
+			public void onSuccess(@NonNull List<MediaContent> items)
+			{
+				// This method is designed to be called from async methods so make sure we've not lost context since then
+				if (getActivity() == null)
+				{
+					return;
+				}
+
+				if (!items.isEmpty() && !TextUtils.isEmpty(items.get(0).getId()))
+				{
+					playPlaylist(items.get(0).getId());
+				}
+				else
+				{
+					getActivity().finish();
+				}
+			}
+
+			@Override
+			public void onFailure(@Nullable Throwable t)
+			{
+				Activity activity = getActivity();
+
+				if (activity != null)
+				{
+					activity.finish();
+					Toast.makeText(activity, "Error: Could not get live playlist", Toast.LENGTH_LONG).show();
+				}
+			}
+		});
+	}
+
+	/**
 	 * Queue a particular Brightcove playlist in the Brightcove player.
 	 *
 	 * @param playlistId
@@ -99,14 +148,46 @@ public class PlaybackFragment extends BrightcovePlayerFragment
 			@Override
 			public void onPlaylist(Playlist playlist)
 			{
-				brightcoveVideoView.addAll(playlist.getVideos());
-				brightcoveVideoView.start();
+				// This method is designed to be called from async methods so make sure we've not lost context since then
+				if (getActivity() == null)
+				{
+					return;
+				}
+
+				VideoInfo videoInfo = MediaManager.getInstance().findCurrentLiveVideo(playlist);
+
+				if (videoInfo != null)
+				{
+					brightcoveVideoView.add(videoInfo.getVideo());
+					brightcoveVideoView.seekTo((int) (System.currentTimeMillis() - videoInfo.getStartTimeUtc()));
+					brightcoveVideoView.start();
+					brightcoveVideoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener()
+					{
+						@Override
+						public void onCompletion(MediaPlayer mediaPlayer)
+						{
+							playLive();
+						}
+					});
+				}
+				else
+				{
+					getActivity().finish();
+				}
 			}
 
 			@Override
 			public void onError(String error)
 			{
 				super.onError(error);
+
+				Activity activity = getActivity();
+
+				if (activity != null)
+				{
+					activity.finish();
+					Toast.makeText(activity, "Error: " + error, Toast.LENGTH_LONG).show();
+				}
 			}
 		});
 	}
@@ -132,8 +213,27 @@ public class PlaybackFragment extends BrightcovePlayerFragment
 			@Override
 			public void onVideo(Video video)
 			{
+				// This method is designed to be called from async methods so make sure we've not lost context since then
+				if (getActivity() == null)
+				{
+					return;
+				}
+
 				brightcoveVideoView.add(video);
 				brightcoveVideoView.start();
+				brightcoveVideoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener()
+				{
+					@Override
+					public void onCompletion(MediaPlayer mediaPlayer)
+					{
+						Activity activity = getActivity();
+
+						if (activity != null)
+						{
+							activity.finish();
+						}
+					}
+				});
 			}
 
 			@Override public void onError(String error)
@@ -144,7 +244,8 @@ public class PlaybackFragment extends BrightcovePlayerFragment
 
 				if (activity != null)
 				{
-					Toast.makeText(activity, "Error: " + error, Toast.LENGTH_SHORT).show();
+					activity.finish();
+					Toast.makeText(activity, "Error: " + error, Toast.LENGTH_LONG).show();
 				}
 			}
 		});
@@ -153,11 +254,20 @@ public class PlaybackFragment extends BrightcovePlayerFragment
 	public void playFile(@NonNull String fileUrl)
 	{
 		brightcoveVideoView.stopPlayback();
-
-		//if (fileUrl.endsWith(".mp4"))
+		brightcoveVideoView.add(Video.createVideo(fileUrl, DeliveryType.MP4));
+		brightcoveVideoView.start();
+		brightcoveVideoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener()
 		{
-			brightcoveVideoView.add(Video.createVideo(fileUrl, DeliveryType.MP4));
-			brightcoveVideoView.start();
-		}
+			@Override
+			public void onCompletion(MediaPlayer mediaPlayer)
+			{
+				Activity activity = getActivity();
+
+				if (activity != null)
+				{
+					activity.finish();
+				}
+			}
+		});
 	}
 }

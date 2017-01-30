c700bbe975ed933589dccd540529b6bf0c1a3362
Tim Matthews <tim@3sidedcube.com>
2017-01-30 14:52:35 +0000

    Fixed crash caused by media items without thumbnails

--- tv/src/main/java/com/cube/lush/player/details/BaseMediaDetailsFragment.java
+++ tv/src/main/java/com/cube/lush/player/details/BaseMediaDetailsFragment.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v17.leanback.app.BrandedFragment;
-import android.util.Log;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -133,9 +133,8 @@ public abstract class BaseMediaDetailsFragment extends BrandedFragment implement
 	 */
 	@Override public void populateHiddenView(@NonNull MediaContent item)
 	{
-		if (backgroundImage != null)
+		if (backgroundImage != null && !TextUtils.isEmpty(item.getThumbnail()))
 		{
-
 			Picasso.with(backgroundImage.getContext())
 				.load(item.getThumbnail())
 				.into(backgroundImage, new Callback()

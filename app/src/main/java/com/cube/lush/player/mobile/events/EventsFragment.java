7395db4b65ffca214f7e155e0300d9d819d8128c
Jamie Cruwys <me@jamiecruwys.co.uk>
2017-03-23 19:52:40 +0000

    Copied classes from mobile module to app module
    Signed-off-by: Jamie Cruwys <me@jamiecruwys.co.uk>

---
+++ app/src/main/java/com/cube/lush/player/mobile/events/EventsFragment.java
@@ -0,0 +1,31 @@
+package com.cube.lush.player.mobile.events;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.cube.lush.player.mobile.R;
+
+public class EventsFragment extends Fragment
+{
+	public EventsFragment()
+	{
+		// Required empty public constructor
+	}
+
+	public static EventsFragment newInstance()
+	{
+		EventsFragment fragment = new EventsFragment();
+		Bundle args = new Bundle();
+		fragment.setArguments(args);
+		return fragment;
+	}
+
+	@Override public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+	{
+		// Inflate the layout for this fragment
+		return inflater.inflate(R.layout.fragment_events, container, false);
+	}
+}

928967ae34b4551d3364fd90bc331bdbcc77b7f8
Jamie Cruwys <jamie.cruwys@3sidedcube.com>
2017-03-23 14:26:06 +0000

    Added placeholder tabs with correct names

--- mobile/src/main/java/com/cube/lush/player/mobile/MainActivity.java
+++ mobile/src/main/java/com/cube/lush/player/mobile/MainActivity.java
@@ -16,18 +16,34 @@ public class MainActivity extends AppCompatActivity
 
 		@Override public boolean onNavigationItemSelected(@NonNull MenuItem item)
 		{
-			switch (item.getItemId())
+			int id = item.getItemId();
+
+			if (id == R.id.navigation_home)
 			{
-				case R.id.navigation_home:
-					mTextMessage.setText(R.string.title_home);
-					return true;
-				case R.id.navigation_dashboard:
-					mTextMessage.setText(R.string.title_dashboard);
-					return true;
-				case R.id.navigation_notifications:
-					mTextMessage.setText(R.string.title_notifications);
-					return true;
+
+				return true;
 			}
+			else if (id == R.id.navigation_live)
+			{
+
+				return true;
+			}
+			else if (id == R.id.navigation_channels)
+			{
+
+				return true;
+			}
+			else if (id == R.id.navigation_events)
+			{
+
+				return true;
+			}
+			else if (id == R.id.navigation_search)
+			{
+
+				return true;
+			}
+
 			return false;
 		}
 

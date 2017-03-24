44d1a4050be2e15fd8329e57b74dde026d2b53cc
Jamie Cruwys <jamie.cruwys@3sidedcube.com>
2017-03-24 10:46:59 +0000

    Fixed base tv activity, which was using appcompat when it shouldn't be

--- app/src/main/java/com/cube/lush/player/tv/base/BaseTVActivity.java
+++ app/src/main/java/com/cube/lush/player/tv/base/BaseTVActivity.java
@@ -1,15 +1,25 @@
 package com.cube.lush.player.tv.base;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 
-import com.cube.lush.player.common.BaseActivity;
 import com.cube.lush.player.tv.search.SearchActivity;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 /**
  * Base activity that performs actions common to all activities in the app.
  */
-public class BaseTVActivity extends BaseActivity
+public class BaseTVActivity extends Activity
 {
+	@Override
+	protected void attachBaseContext(Context newBase)
+	{
+		// Context wrapper to apply custom fonts
+		super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+	}
+
 	@Override
 	public boolean onSearchRequested()
 	{

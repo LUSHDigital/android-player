74778fdd00b8e030ea17ef8a6efe7c9d0b768b8b
Jamie Cruwys <jamie.cruwys@3sidedcube.com>
2017-03-24 15:09:04 +0000

    Added loading text

--- app/src/main/java/com/cube/lush/player/mobile/SpinnerFragment.java
+++ app/src/main/java/com/cube/lush/player/mobile/SpinnerFragment.java
@@ -18,6 +18,6 @@ public class SpinnerFragment extends Fragment
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
 	{
-		return inflater.inflate(R.layout.fragment_spinner, container, false);
+		return inflater.inflate(R.layout.fragment_loading, container, false);
 	}
 }

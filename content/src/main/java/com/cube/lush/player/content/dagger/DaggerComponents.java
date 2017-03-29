6c590baffc569e00a535c25865b6fd7fc9d6529d
Jamie Cruwys <jamie.cruwys@3sidedcube.com>
2017-03-29 17:35:04 +0100

    We now have mocked api endpoints. At the moment they don't respond to the correct endpoints, but they work

--- content/src/main/java/com/cube/lush/player/content/dagger/DaggerComponents.java
+++ content/src/main/java/com/cube/lush/player/content/dagger/DaggerComponents.java
@@ -1,5 +1,8 @@
 package com.cube.lush.player.content.dagger;
 
+import android.content.Context;
+import android.support.annotation.Nullable;
+
 import com.cube.lush.player.content.dagger.api.APIComponent;
 import com.cube.lush.player.content.dagger.api.APIModule;
 import com.cube.lush.player.content.dagger.api.DaggerAPIComponent;
@@ -15,10 +18,21 @@ public class DaggerComponents
 	private static final String BASE_URL = "http://admin.player.lush.com/lushtvapi/v1/views/";
 
 	private static DaggerComponents instance;
+	private static Context context;
 	@Getter protected APIComponent api;
 
 	public static DaggerComponents getInstance()
 	{
+		return getInstance(null);
+	}
+
+	public static DaggerComponents getInstance(@Nullable Context currentContext)
+	{
+		if (currentContext != null)
+		{
+			context = currentContext;
+		}
+
 		if (instance == null)
 		{
 			instance = new DaggerComponents();
@@ -30,7 +44,7 @@ public class DaggerComponents
 	private DaggerComponents()
 	{
 		api = DaggerAPIComponent.builder()
-			.aPIModule(new APIModule(BASE_URL))
+			.aPIModule(new APIModule(context, BASE_URL))
 			.build();
 	}
 }

f41cebf867055d151ab1a32b157d3dc5d1e894cb
Jamie Cruwys <jamie.cruwys@3sidedcube.com>
2017-03-29 13:09:41 +0100

    Changed home and live layouts to be prefixed with mobile

--- app/src/main/java/com/cube/lush/player/mobile/home/HomeFragment.java
+++ app/src/main/java/com/cube/lush/player/mobile/home/HomeFragment.java
@@ -26,6 +26,6 @@ public class HomeFragment extends Fragment
 	@Override public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
 	{
 		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.fragment_home, container, false);
+		return inflater.inflate(R.layout.mobile_fragment_home, container, false);
 	}
 }

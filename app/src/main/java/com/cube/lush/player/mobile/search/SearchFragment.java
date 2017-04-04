90fb7d8f44a1517bae4dd08250155b261ff3c42c
Jamie Cruwys <jamie.cruwys@3sidedcube.com>
2017-04-04 10:09:46 +0100

    Added search view

--- app/src/main/java/com/cube/lush/player/mobile/search/SearchFragment.java
+++ app/src/main/java/com/cube/lush/player/mobile/search/SearchFragment.java
@@ -1,15 +1,28 @@
 package com.cube.lush.player.mobile.search;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.SearchView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.cube.lush.player.R;
+import com.cube.lush.player.api.model.SearchResult;
+import com.cube.lush.player.content.handler.ResponseHandler;
+import com.cube.lush.player.content.manager.SearchManager;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class SearchFragment extends Fragment
 {
+	@BindView(R.id.search) SearchView searchView;
+
 	public SearchFragment()
 	{
 		// Required empty public constructor
@@ -25,7 +38,52 @@ public class SearchFragment extends Fragment
 
 	@Override public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
 	{
-		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.mobile_fragment_search, container, false);
+		View view = inflater.inflate(R.layout.mobile_fragment_search, container, false);
+		ButterKnife.bind(this, view);
+
+		return view;
+	}
+
+	@Override public void onActivityCreated(@Nullable Bundle savedInstanceState)
+	{
+		super.onActivityCreated(savedInstanceState);
+
+		if (savedInstanceState == null)
+		{
+			populateUi();
+		}
+	}
+
+	private void populateUi()
+	{
+		// Make it default to the expanded state
+		searchView.onActionViewExpanded();
+
+		// Set the listener to get data on submit
+		searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener()
+		{
+			@Override public boolean onQueryTextSubmit(String query)
+			{
+				SearchManager.getInstance().search(query, new ResponseHandler<SearchResult>()
+				{
+					@Override public void onSuccess(@NonNull List<SearchResult> items)
+					{
+						// TODO: Implement success
+					}
+
+					@Override public void onFailure(@Nullable Throwable t)
+					{
+						// TODO: Implement failure
+					}
+				});
+
+				return true;
+			}
+
+			@Override public boolean onQueryTextChange(String newText)
+			{
+				return false;
+			}
+		});
 	}
 }

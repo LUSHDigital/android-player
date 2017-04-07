638c239ee83c0b23dec331725b982b0948f7ebe0
Jamie Cruwys <jamie.cruwys@3sidedcube.com>
2017-04-07 10:53:16 +0100

    Removed old spinner fragment that would show/hide. We now have statefulviews that does this for us

--- app/src/main/java/com/cube/lush/player/mobile/MainActivity.java
+++ app/src/main/java/com/cube/lush/player/mobile/MainActivity.java
@@ -126,32 +126,6 @@ public class MainActivity extends BaseMobileActivity implements AHBottomNavigati
 		transaction.commit();
 	}
 
-	public void showLoading()
-	{
-		getSupportFragmentManager().beginTransaction()
-			.add(container.getId(), new SpinnerFragment(), SpinnerFragment.SPINNER_FRAGMENT_TAG)
-			.commit();
-	}
-
-	public void hideLoading()
-	{
-		FragmentManager fragmentManager = getSupportFragmentManager();
-
-		if (fragmentManager.isDestroyed())
-		{
-			return;
-		}
-
-		Fragment fragment = fragmentManager.findFragmentByTag(SpinnerFragment.SPINNER_FRAGMENT_TAG);
-
-		if (fragment != null)
-		{
-			fragmentManager.beginTransaction()
-				.remove(fragment)
-				.commit();
-		}
-	}
-
 	@Override public boolean onKeyDown(int keyCode, KeyEvent event)
 	{
 		if (keyCode == KeyEvent.KEYCODE_BACK)

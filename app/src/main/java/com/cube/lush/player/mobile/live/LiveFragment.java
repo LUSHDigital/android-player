147fca9323b1db1369069e9b2586e833c18b496d
Jamie Cruwys <jamie.cruwys@3sidedcube.com>
2017-04-07 12:30:45 +0100

    Live tab now show empty state

--- app/src/main/java/com/cube/lush/player/mobile/live/LiveFragment.java
+++ app/src/main/java/com/cube/lush/player/mobile/live/LiveFragment.java
@@ -1,16 +1,26 @@
 package com.cube.lush.player.mobile.live;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
 
 import com.cube.lush.player.R;
+import com.cube.lush.player.api.model.MediaContent;
+import com.cube.lush.player.content.handler.ResponseHandler;
+import com.cube.lush.player.content.manager.MediaManager;
+import com.cube.lush.player.mobile.base.BaseAdapter;
+import com.cube.lush.player.mobile.base.ListDataRetrieval;
+import com.cube.lush.player.mobile.base.ListingFragment;
+import com.cube.lush.player.mobile.base.RecyclerViewClickedListener;
+import com.cube.lush.player.mobile.content.adapter.ContentAdapter;
 
-import uk.co.jamiecruwys.StatefulFragment;
+import java.util.Collections;
+import java.util.List;
 
-public class LiveFragment extends StatefulFragment
+public class LiveFragment extends ListingFragment implements RecyclerViewClickedListener<MediaContent>
 {
 	public LiveFragment()
 	{
@@ -23,6 +33,16 @@ public class LiveFragment extends StatefulFragment
 		Bundle args = new Bundle();
 		fragment.setArguments(args);
 		return fragment;
+	}
+
+	@NonNull @Override protected RecyclerView.LayoutManager provideLayoutManager()
+	{
+		return new LinearLayoutManager(getContext());
+	}
+
+	@NonNull @Override protected BaseAdapter provideAdapter()
+	{
+		return new ContentAdapter(Collections.EMPTY_LIST, this);
 	}
 
 	@Override public int provideContentLayout()
@@ -43,5 +63,26 @@ public class LiveFragment extends StatefulFragment
 	@Override public int provideErrorLayout()
 	{
 		return R.layout.mobile_error;
+	}
+
+	@Override protected void getListData(@NonNull final ListDataRetrieval callback)
+	{
+		MediaManager.getInstance().getLiveContent(new ResponseHandler<MediaContent>()
+		{
+			@Override public void onSuccess(@NonNull List<MediaContent> items)
+			{
+				callback.onListDataRetrieved(items);
+			}
+
+			@Override public void onFailure(@Nullable Throwable t)
+			{
+				callback.onListDataRetrievalError(t);
+			}
+		});
+	}
+
+	@Override public void onRecyclerViewItemClicked(@NonNull MediaContent item)
+	{
+		Toast.makeText(getContext(), "Live item clicked: " + item.getTitle(), Toast.LENGTH_SHORT).show();
 	}
 }

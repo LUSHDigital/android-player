089be3798d462213282e99e824e7ab85523d4689
Jamie Cruwys <jamie.cruwys@3sidedcube.com>
2017-04-07 10:37:59 +0100

    Stateful events

--- app/src/main/java/com/cube/lush/player/mobile/events/EventsFragment.java
+++ app/src/main/java/com/cube/lush/player/mobile/events/EventsFragment.java
@@ -1,14 +1,26 @@
 package com.cube.lush.player.mobile.events;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.cube.lush.player.R;
+import com.cube.lush.player.api.model.MediaContent;
+import com.cube.lush.player.mobile.base.BaseAdapter;
+import com.cube.lush.player.mobile.base.ListDataRetrieval;
+import com.cube.lush.player.mobile.base.ListingFragment;
+import com.cube.lush.player.mobile.base.RecyclerViewClickedListener;
+import com.cube.lush.player.mobile.events.adapter.EventsAdapter;
 
-public class EventsFragment extends Fragment
+import java.util.Collections;
+
+public class EventsFragment extends ListingFragment implements RecyclerViewClickedListener<MediaContent>
 {
 	public EventsFragment()
 	{
@@ -23,9 +35,23 @@ public class EventsFragment extends Fragment
 		return fragment;
 	}
 
-	@Override public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+	@NonNull @Override protected RecyclerView.LayoutManager provideLayoutManager()
 	{
-		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.mobile_fragment_events, container, false);
+		return new LinearLayoutManager(getContext());
+	}
+
+	@NonNull @Override protected BaseAdapter provideAdapter()
+	{
+		return new EventsAdapter(Collections.EMPTY_LIST, this);
+	}
+
+	@Override protected void getListData(@NonNull ListDataRetrieval callback)
+	{
+		callback.onListDataRetrieved(Collections.EMPTY_LIST);
+	}
+
+	@Override public void onRecyclerViewItemClicked(@NonNull MediaContent item)
+	{
+		Toast.makeText(getContext(), "Selected event: " + item.getTitle(), Toast.LENGTH_SHORT);
 	}
 }
---
+++ app/src/main/java/com/cube/lush/player/mobile/events/adapter/EventsAdapter.java
@@ -0,0 +1,50 @@
+package com.cube.lush.player.mobile.events.adapter;
+
+import android.support.annotation.NonNull;
+import android.view.View;
+
+import com.cube.lush.player.R;
+import com.cube.lush.player.api.model.MediaContent;
+import com.cube.lush.player.mobile.base.BaseAdapter;
+import com.cube.lush.player.mobile.base.RecyclerViewClickedListener;
+import com.cube.lush.player.mobile.events.holder.EventViewHolder;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+/**
+ * Created by Jamie Cruwys of 3 SIDED CUBE on 23/03/2017.
+ */
+public class EventsAdapter extends BaseAdapter<MediaContent, EventViewHolder>
+{
+	private RecyclerViewClickedListener listener;
+
+	public EventsAdapter(@NonNull List<MediaContent> items, @NonNull RecyclerViewClickedListener listener)
+	{
+		super(items);
+		this.listener = listener;
+	}
+
+	@Override protected int provideViewHolderLayout()
+	{
+		return R.layout.mobile_item_content;
+	}
+
+	@NonNull @Override protected EventViewHolder createViewHolder(@NonNull View itemView)
+	{
+		return new EventViewHolder(itemView, listener);
+	}
+
+	@Override protected void bind(@NonNull EventViewHolder holder, @NonNull MediaContent item)
+	{
+		holder.type.setText(item.getType().getName());
+		holder.title.setText(item.getTitle());
+		holder.length.setText(item.getRelativeDate());
+
+		Picasso.with(holder.image.getContext())
+			.load(item.getThumbnail())
+			.fit()
+			.centerCrop()
+			.into(holder.image);
+	}
+}

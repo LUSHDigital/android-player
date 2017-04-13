1a362fb0d08479fe7a4258b6f1346a28cf7ad954
Jamie Cruwys <jamie.cruwys@3sidedcube.com>
2017-04-13 15:27:02 +0100

    Optional null listener for base view holder

--- app/src/main/java/com/cube/lush/player/mobile/base/BaseViewHolder.java
+++ app/src/main/java/com/cube/lush/player/mobile/base/BaseViewHolder.java
@@ -1,6 +1,7 @@
 package com.cube.lush.player.mobile.base;
 
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
@@ -13,17 +14,24 @@ public class BaseViewHolder<ITEMTYPE> extends RecyclerView.ViewHolder implements
 	private ITEMTYPE item;
 	private RecyclerViewClickedListener<ITEMTYPE> listener;
 
-	public BaseViewHolder(@NonNull View itemView, @NonNull final RecyclerViewClickedListener<ITEMTYPE> listener)
+	public BaseViewHolder(@NonNull View itemView, @Nullable final RecyclerViewClickedListener<ITEMTYPE> listener)
 	{
 		super(itemView);
 		this.listener = listener;
-		itemView.setOnClickListener(this);
+
+		if (listener != null)
+		{
+			itemView.setOnClickListener(this);
+		}
 	}
 
 	@Override
 	public void onClick(View view)
 	{
-		listener.onRecyclerViewItemClicked(item);
+		if (listener != null)
+		{
+			listener.onRecyclerViewItemClicked(item);
+		}
 	}
 
 	public void setItem(@NonNull ITEMTYPE item)

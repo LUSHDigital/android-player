efb5f8a69d81d3d5fbb1141cae7dacbfb8554834
Jamie Cruwys <jamie.cruwys@3sidedcube.com>
2017-04-20 09:41:40 +0100

    Fixed showcase name

--- app/src/main/java/com/cube/lush/player/mobile/events/EventTab.java
+++ app/src/main/java/com/cube/lush/player/mobile/events/EventTab.java
@@ -21,7 +21,7 @@ public enum EventTab
 	SUMMIT("Lush Summit 2017", "summit"),
 
 	@SuppressWarnings("HardCodedStringLiteral")
-	SHOWCASE("Creative Showcase", "showcase 2016");
+	SHOWCASE("Creative Showcase 2016", "showcase 2016");
 
 	/**
 	 * The name used for the tabs in the UI

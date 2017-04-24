3d7a50dd75ac3976fa7e812aab301e88714b4722
Jamie Cruwys <jamie.cruwys@3sidedcube.com>
2017-04-24 11:16:46 +0100

    Remove filtering options from home tab

--- app/src/main/java/com/cube/lush/player/mobile/home/HomeFragment.java
+++ app/src/main/java/com/cube/lush/player/mobile/home/HomeFragment.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
@@ -13,19 +12,19 @@ import com.cube.lush.player.api.model.MediaContent;
 import com.cube.lush.player.content.handler.ResponseHandler;
 import com.cube.lush.player.content.manager.MediaManager;
 import com.cube.lush.player.mobile.MainActivity;
-import com.cube.lush.player.mobile.base.FilterableListingFragment;
 import com.cube.lush.player.mobile.content.adapter.ContentAdapter;
 import com.cube.lush.player.mobile.details.DetailsFragment;
 import com.lush.lib.listener.OnListItemClickListener;
 
 import java.util.List;
 
+import uk.co.jamiecruwys.StatefulListingFragment;
 import uk.co.jamiecruwys.contracts.ListingData;
 
 /**
  * Created by Jamie Cruwys.
  */
-public class HomeFragment extends FilterableListingFragment<MediaContent, HomeTab> implements OnListItemClickListener<MediaContent>
+public class HomeFragment extends StatefulListingFragment<MediaContent> implements OnListItemClickListener<MediaContent>
 {
 	public HomeFragment()
 	{
@@ -40,14 +39,20 @@ public class HomeFragment extends FilterableListingFragment<MediaContent, HomeTa
 		return fragment;
 	}
 
-	@NonNull @Override public List<HomeTab> provideFilterOptions()
+	@NonNull @Override protected RecyclerView.LayoutManager provideLayoutManager()
 	{
-		return HomeTab.listValues();
+		final int NUMBER_COLUMNS = getResources().getInteger(R.integer.home_columns);
+		return new GridLayoutManager(getContext(), NUMBER_COLUMNS);
 	}
 
-	@Override public void getListDataForFilterOption(@NonNull final HomeTab homeTab, @NonNull final ListingData callback)
+	@NonNull @Override protected RecyclerView.Adapter provideAdapter(@NonNull List<MediaContent> items)
 	{
-		MediaManager.getInstance().getContentForTag(homeTab.getTag(), 50, new ResponseHandler<MediaContent>()
+		return new ContentAdapter(items, this);
+	}
+
+	@Override protected void getListData(@NonNull final ListingData callback)
+	{
+		MediaManager.getInstance().getAllContent(new ResponseHandler<MediaContent>()
 		{
 			@Override public void onSuccess(@NonNull List<MediaContent> items)
 			{
@@ -59,32 +64,6 @@ public class HomeFragment extends FilterableListingFragment<MediaContent, HomeTa
 				callback.onListingDataError(t);
 			}
 		});
-	}
-
-	@NonNull @Override public String getTitleForFilterOption(HomeTab homeTab)
-	{
-		return homeTab.getDisplayName();
-	}
-
-	@NonNull @Override public HomeTab provideDefaultTab()
-	{
-		return HomeTab.ALL;
-	}
-
-	@NonNull @Override public LinearLayoutManager provideLayoutManagerForFilterOption(HomeTab homeTab)
-	{
-		final int NUMBER_COLUMNS = getResources().getInteger(R.integer.home_columns);
-		return new GridLayoutManager(getContext(), NUMBER_COLUMNS);
-	}
-
-	@NonNull @Override public RecyclerView.Adapter provideAdapterForFilterOption(HomeTab homeTab, @NonNull List<MediaContent> items)
-	{
-		return new ContentAdapter(items, this);
-	}
-
-	@Nullable @Override public RecyclerView.ItemDecoration provideItemDecorationForFilterOption(HomeTab homeTab)
-	{
-		return null;
 	}
 
 	@Override public int provideLoadingLayout()

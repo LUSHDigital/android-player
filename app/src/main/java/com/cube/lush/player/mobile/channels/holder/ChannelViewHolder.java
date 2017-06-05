b9641f7f463e3c39e05175eeaf149de4dc5d0e5f
Jamie Cruwys <jamie.cruwys@3sidedcube.com>
2017-06-05 17:41:00 +0100

    Channel images and fallbacks

--- app/src/main/java/com/cube/lush/player/mobile/channels/holder/ChannelViewHolder.java
+++ app/src/main/java/com/cube/lush/player/mobile/channels/holder/ChannelViewHolder.java
@@ -1,11 +1,14 @@
 package com.cube.lush.player.mobile.channels.holder;
 
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.cube.lush.player.R;
 import com.cube.lush.player.api.model.Channel;
 import com.lush.view.holder.BaseViewHolder;
+import com.squareup.picasso.Picasso;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -18,6 +21,7 @@ import butterknife.ButterKnife;
 public class ChannelViewHolder extends BaseViewHolder<Channel>
 {
 	@BindView(R.id.image) public ImageView image;
+	@BindView(R.id.alternative_text) public TextView alternativeText;
 
 	public ChannelViewHolder(View view)
 	{
@@ -27,9 +31,19 @@ public class ChannelViewHolder extends BaseViewHolder<Channel>
 
 	@Override public void bind(Channel channel)
 	{
-		// channel.getImage()
-		// TODO: Load channel.getImage() into the image view
-		// Drawable drawable = ContextCompat.getDrawable(image.getContext(), channel.getLogo());
-		// image.setImageDrawable(drawable);
+		String imageUri = channel.getImage();
+
+		if (TextUtils.isEmpty(imageUri))
+		{
+			image.setVisibility(View.GONE);
+
+			alternativeText.setText(channel.getName());
+			alternativeText.setVisibility(View.VISIBLE);
+			return;
+		}
+
+		Picasso.with(this.image.getContext())
+			.load(imageUri)
+			.into(image);
 	}
 }
